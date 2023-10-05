749144e8ddb707ec6372a20bfa5490b69270d2da
orajiakuchibuchi <somtobuchi@gmail.com>
2023-10-05 11:11:43 +0100

    first commit

--- server.js
+++ server.js
@@ -1,31 +1,43 @@
-// Server Side Rendering 
+// Server Side Rendering
 // An express nodejs server by Orajiaku Chibuchi Somto https://www.linkedin.com/in/orajiaku-c-0a051815a/
 // Lunch node server.js to start server 
+const SEC_PORT = '5001';
+const NODE_ENV_PORT = process.env.PORT || process.env.NODE_ENV;
+const PORT = NODE_ENV_PORT || SEC_PORT;
 
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const http =  require('http');
 const compression = require('compression');
+const _io = require('socket.io');
 const app = express();
+const server = http.createServer(app);
 
-const SEC_PORT = '5001';
 // where '/dist/admin' is the final built directory
 const staticRoot = __dirname + '/Sparklingv2/';
 
-const NODE_ENV_PORT = process.env.PORT || process.env.NODE_ENV;
-const PORT = NODE_ENV_PORT || SEC_PORT;
-
 app.set('port', PORT);
+
+const io = _io(server,  {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }
+  })
 
 // Compression middleware
 app.use(compression());
-
 /* other middleware */
+
+io.on('connection', (socket) => { console.log(`connected ${socket.id}`)});
 
 /* place any backend routes you have here */
 
 app.use(function(req, res, next) {
-  
+
     //if the request is not html then move along
     var accept = req.accepts('html', 'json', 'xml');
     if (accept !== 'html') {
@@ -44,6 +56,7 @@ app.use(function(req, res, next) {
 
 app.use(express.static(staticRoot));
 
-app.listen(app.get('port'), function() {
+// app.listen wont work as it creates a new app!!
+server.listen(app.get('port'), function() {
     console.log('app running on port', app.get('port'));
 });
